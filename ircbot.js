feccc99cb1d115bcae40bca3b046213876fea774
Ivaylo Markov <ivo@ludost.net>
2016-09-07 21:58:27 +0300

    Add weather reporting

--- ircbot.js
+++ ircbot.js
@@ -3,6 +3,7 @@
 var irc = require('irc');
 var fs = require('fs');
 var http = require('http');
+var request = require('request');
 
 var control = require('./control');
 
@@ -55,6 +56,25 @@ function ircConnect() {
 ircbot.addListener('action', function(from, to, message) {
     if (to.indexOf('#') === 0 && message === 'kicks ' + config.irc.nickname) {
         ircbot.say(to, 'ouch!');
+    }
+});
+
+// weather
+ircbot.addListener('message', function (from, to, message) {
+    if (to === config.irc.announceChannel && message === '!weather') {
+        request({
+            url: 'https://cassie.initlab.org/weather.json',
+            json: true
+        }, function(error, response, body) {
+            if (error !== null || response.statusCode !== 200) {
+                ircbot.say(to, 'Error getting data');
+                return;
+            }
+            
+            ircbot.say(to, 'Temperature: ' + body.temp_in.toFixed(1) + ' °C in / ' + body.temp_out.toFixed(1) + ' °C out');
+            ircbot.say(to, 'Humidity: ' + body.hum_in.toFixed(0) + ' % in / ' + body.hum_out.toFixed(0) + ' % out');
+            ircbot.say(to, 'Pressure: ' + body.abs_pressure.toFixed(1) + ' hPa');
+        });
     }
 });
 
