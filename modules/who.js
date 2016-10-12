8010e191e750a0275d7ac3c343d595043b97974f
Ivaylo Markov <ivo@ludost.net>
2016-10-12 19:45:32 +0300

    Count mysterious users

--- modules/who.js
+++ modules/who.js
@@ -15,16 +15,22 @@ module.exports = function(ircbot, config) {
 				return;
 			}
 			
+            var mystery_users = 0;
 			var people = body.map(function(user) {
-				return user.name + ' (' + user.username + ')';
+                if (user.username != 'mystery_user') { return user.name + ' (' + user.username + ')'; }
+                else { mystery_users++; }
 			});
 			
 			if (people.length === 0) {
 				ircbot.say(to, 'No one in init Lab :(');
 				return;
 			}
+
+            var mystery_str = '';
+            if (mystery_users > 1) {  mystery_str = mystery_users + ' Mystery labbers'; }
+            else { mystery_str = 'Mystery labber'}
 			
-			ircbot.say(to, 'People in init Lab: ' + people.join(', '));
+			ircbot.say(to, 'People in init Lab: ' + people.join(', ') + ', ' + mystery_str);
 		});
 	});
 };
