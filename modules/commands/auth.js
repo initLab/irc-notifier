e759865d2c5b4baa3ab8fff64644c8941a2416a2
Vencislav Atanasov <user890104@users.noreply.github.com>
2017-01-08 20:35:09 +0200

    Fix auth command

--- modules/commands/auth.js
+++ modules/commands/auth.js
@@ -4,9 +4,14 @@ module.exports = {
 	key: 'auth',
 	description: 'checks if user is logged in',
 	execute: function(ircbot, config, utils, from, to, message) {
-		ircbot.whois(from, function(info) {
+		ircbot.remoteWhois(from, function(info) {
 			if ('account' in info) {
-				ircbot.say(to, from + ' is logged in as ' + info.account);
+				if ('secure_connection' in info) {
+					ircbot.say(to, from + ' is logged in as ' + info.account + ' and using TLS');
+				}
+				else {
+					ircbot.say(to, from + ' is logged in as ' + info.account + ' and NOT using TLS');
+				}
 			}
 			else {
 				ircbot.say(to, from + ' is not logged in');
