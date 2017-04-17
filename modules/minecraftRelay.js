a85039a91431b734704000b8bc1e200d9b4ae642
Vencislav Atanasov <user890104@users.noreply.github.com>
2017-04-17 21:55:31 +0300

    Add module for relaying IRC messages to Minecraft using RCON

---
+++ modules/minecraftRelay.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(config, ircbot) {
+	const Rcon = require('rcon');
+
+	const client = new Rcon(config.rcon.host, config.rcon.port, config.rcon.password);
+	
+	client.on('auth', function() {
+		console.log('RCON authentication successful');
+	}).on('response', function(str) {
+		console.log('RCON got response: ' + str);
+	}).on('end', function() {
+		console.log('RCON socket closed');
+	});
+
+	client.connect();
+	
+	ircbot.addListener('action', function(from, to, text) {
+		if (to !== config.channel) {
+			return;
+		}
+		
+		client.send('say IRC> * ' + from + ' ' + text);
+	});
+	
+	ircbot.addListener('message' + config.channel, function(from, text) {
+		client.send('say IRC> <' + from + '> ' + text);
+	});
+};
