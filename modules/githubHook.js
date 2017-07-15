34139dd12649f1c8951a8cb2c96be0626c0230c2
Vencislav Atanasov <user890104@users.noreply.github.com>
2017-07-16 01:00:42 +0300

    Add prototype for github hook

---
+++ modules/githubHook.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const crypto = require('crypto');
+
+module.exports = function(config, ircbot) {
+	ircbot.emit('registerHttp', 'post', '/hooks/github', function(req, res) {
+		const signature = req.headers['x-hub-signature'];
+		const algo = signature.substr(0, signature.indexOf('='));
+		
+		const expectedSignature = algo + '=' +
+			crypto.createHmac(algo, config.secret).update(req.body).digest('hex');
+		
+		if (signature !== expectedSignature) {
+			res.writeHead(400, {
+				'Content-Type': 'text/plain'
+			});
+			res.end('Bad request');
+			return;
+		}
+		
+		const event = req.headers['x-github-event'];
+		const message = JSON.parse(req.body);
+		
+		res.writeHead(204, {
+			'Content-Type': 'text/plain'
+		});
+		res.end();
+		
+		ircbot.say(config.announceChannel, '[' + message.repository.name + '] ' + message.sender.login + ' triggered event ' + event);
+	});
+};
