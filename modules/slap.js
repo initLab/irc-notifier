4c394d7edfc56fe998cd838b37ce4e9fe1ae8e34
Vencislav Atanasov <user890104@users.noreply.github.com>
2019-01-01 16:15:31 +0200

    Fix easter egg

--- modules/slap.js
+++ modules/slap.js
@@ -2,7 +2,7 @@
 
 module.exports = function(config, ircbot) {
 	ircbot.addListener('action', function(from, to, message) {
-		if (to.indexOf('#') === 0 && message === 'kicks ' + ircbot.opt.nickname) {
+		if (to.indexOf('#') === 0 && message.indexOf('kicks ' + ircbot.opt.nick) === 0) {
 			ircbot.say(to, 'ouch!');
 		}
 	});
