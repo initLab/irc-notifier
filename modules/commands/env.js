ee6d973f10e21b439a6b8c6ec4aed4c32282e1bd
Vencislav Atanasov <git@6bez10.info>
2022-02-13 01:07:20 +0200

    Rename restart command parameter, do not exit the app when restarting

--- modules/commands/env.js
+++ modules/commands/env.js
@@ -8,44 +8,44 @@ module.exports = function(config, ircbot, utils) {
 		'power': 'W',
 		'energy': 'kWh'
 	};
-	
+
 	function execute(replyTo) {
 		utils.request.getJson(config.url, function(data) {
 			for (let sensor in config.sensors) {
 				let values = [];
-				
+
 				Object.keys(UNITS).forEach(function(unit) {
 					const key = sensor + '/' + unit;
-					
+
 					if (!(key in data)) {
 						return;
 					}
-					
+
 					const currValue = data[key];
 					const label = unit[0].toUpperCase() + unit.substr(1);
 					const delta = Math.max(0, Date.now() - currValue.timestamp) / 1000;
 
 					let message = label + ': ' +
 						parseFloat(currValue.value).toFixed(1) + UNITS[unit];
-					
+
 					if (delta > config.timeout) {
 						message += ' (' + utils.time.formatTimePeriod(delta, true, 'ago') + ')';
 					}
-					
+
 					values.push(message);
 				});
-				
+
 				if (values.length === 0) {
 					continue;
 				}
-				
+
 				ircbot.say(replyTo, config.sensors[sensor].name + ': ' + values.join(' / '));
 			}
 		}, function(error) {
 			ircbot.say(replyTo, 'Env error: ' + error);
 		});
 	}
-	
+
 	return {
 		key: 'env',
 		description: 'shows sensor readings',
--- modules/commands/update.js
+++ modules/commands/update.js
@@ -5,11 +5,11 @@ module.exports = function(config, ircbot, utils) {
 		const child_process = require('child_process');
 
 		let needsUpdate = true;
-		
+
 		const git = child_process.spawn('git', ['pull', '--ff-only', '--no-stat'], {
 			shell: true
 		});
-		
+
 		git.stdout.on('data', (data) => {
 			if (data.toString() === 'Already up-to-date.\n') {
 				needsUpdate = false;
@@ -23,40 +23,39 @@ module.exports = function(config, ircbot, utils) {
 		git.stderr.on('data', (data) => {
 			ircbot.say(replyTo, data);
 		});
-		
+
 		git.on('close', (code) => {
 			if (code !== 0) {
 				ircbot.say(replyTo, `Failed to update: child process exited with code ${code}`);
 				return;
 			}
-			
+
 			if (!needsUpdate) {
 				return;
 			}
-			
+
 			if ('startupScript' in config) {
 				ircbot.say(replyTo, 'Updated successfully, restarting...');
-				
+
 				setTimeout(function() {
-					const newInstance = child_process.spawn(config.startupScript, {
+					const newInstance = child_process.spawn(config.restartCommand, {
 						stdio: 'ignore',
 						detached: true
 					});
-					
+
 					newInstance.unref();
-					process.exit();
 				}, 1000);
 			}
 			else {
 				ircbot.say(replyTo, 'Updated successfully, please restart.');
 			}
 		});
-		
+
 		git.on('error', (err) => {
 			ircbot.say(replyTo, `Failed to update: ${err}`);
 		});
 	}
-	
+
 	return {
 		key: 'update',
 		description: 'updates the bot',
