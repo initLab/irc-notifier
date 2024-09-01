f14d21ad80bbb1509bfe0ab136b41b7ad6ef9613
Vencislav Atanasov <git@6bez10.info>
2024-09-01 18:16:24 +0300

    Bring back the current music track command

---
+++ modules/commands/music.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function(config, ircbot, utils) {
+	function execute(replyTo) {
+		// noinspection HttpUrlsUsage
+		utils.request.getJson(config.url, function(data) {
+			if (data.error) {
+				ircbot.say('Error reading music status: ' + data.error);
+				return;
+			}
+
+			const states = {
+				play: 'playing',
+				pause: 'paused',
+				stop: 'stopped',
+			};
+
+			ircbot.say(replyTo,
+				'[' + states[data.status.state] + ']' + (data.currentSong ? (' ' + (
+					data.currentSong.Artist ? (data.currentSong.Artist + ' - ') : ''
+				) + (
+					data.currentSong.Title || ''
+				) + (
+					data.currentSong.Album ? (' (' + data.currentSong.Album + (
+						data.currentSong.Date ? (', ' + data.currentSong.Date) : ''
+					) + ')') : ''
+				)) : '')
+			);
+		}, function(error) {
+			ircbot.say(replyTo, error);
+		});
+	}
+
+	return {
+		keys: ['music'],
+		description: 'shows current music track',
+		execute: execute
+	};
+};
